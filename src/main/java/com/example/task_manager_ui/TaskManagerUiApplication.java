77f1e22b5661e0414d2e75b1bd2d01c186192e62
Yevhenii Rud <zhenniarud@gmail.com>
2024-10-06 14:23:25 +0300

    Initial commit

---
+++ src/main/java/com/example/task_manager_ui/TaskManagerUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/task_manager_ui/TaskManagerUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
