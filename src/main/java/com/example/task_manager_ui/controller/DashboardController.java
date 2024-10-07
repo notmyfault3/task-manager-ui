3645a37c8f4aeca2f4064280bcbde552ad6336f5
Yevhenii Rud <zhenniarud@gmail.com>
2024-10-07 16:45:30 +0300

    Add initial DashboardController

---
+++ src/main/java/com/example/task_manager_ui/controller/DashboardController.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String dashboardPage() {
+        return "dashboard";
+    }
+}
